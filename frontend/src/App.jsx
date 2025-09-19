4bfa731d08dd5445009675de2a01ceaf8455f35f
parth17-code <2024.parth.mohite@ves.ac.in>
2025-09-19 20:27:52 +0530

    LoginForm import directory corrected in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import LoginForm from './portals/migrant/login'
+import LoginForm from './portals/hospital/login'
 
 function App() {
   const [count, setCount] = useState(0)
